9e9440501e3d87ec471e6ab5a28d2b9493557e28
Muhammed Saryyew <nsaryyev9797@gmail.com>
2021-12-29 15:54:10 +0200

    utils was created

---
+++ src/classes/index.ts
@@ -0,0 +1,9 @@
+import User from './User'
+import Company from './Company'
+import Map from './Map'
+
+export {
+    User,
+    Company,
+    Map
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import User from './classes/User'
+import { User } from './classes'
 
 const user = new User();
 
